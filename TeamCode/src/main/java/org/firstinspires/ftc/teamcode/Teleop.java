024edb7deb1c6a9015b195933af2d2ee97e05521
willtheboi <william.fleming@ums.legacypca.org>
2026-02-27 10:46:13 -0600

    edited TeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -123,13 +123,13 @@ public abstract class Teleop extends OpMode {
         else if (gamepad2.dpad_down) {
             hood.setPosition(-1);
         }
-        launcher1.setVelocity(gamepad2.left_trigger*-1400);
-        launcher2.setVelocity(gamepad2.left_trigger*-1400);
+        launcher1.setVelocity(gamepad2.left_trigger*-1700);
+        launcher2.setVelocity(gamepad2.left_trigger*-1700);
 
-        /*double strafe = gamepad1.right_trigger - gamepad1.left_trigger;
+        double strafe = gamepad1.right_trigger - gamepad1.left_trigger;
         drive(-gamepad1.left_stick_y, -strafe, gamepad1.right_stick_x);
 
-        LPID(targetVelocity, launcher.getVelocity());*/
+        /*LPID(targetVelocity, launcher.getVelocity());*/
     }
 
     /*public void LPID(double target, double current) {
